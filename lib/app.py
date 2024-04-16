2d21cc0f079ece98c87a021bbbeeacfd3955a9dc
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-04-15 22:41:33 -0600

    going to bed

--- lib/app.py
+++ lib/app.py
@@ -49,6 +49,18 @@ class Company:
         else:
             raise ValueError("Discontinued models must be a number")
 
+    def all_companies():
+        cursor.execute(
+            '''
+            SELECT name FROM companies
+            '''
+        )
+        companies_all = cursor.fetchall()
+        if companies_all:
+            return [company[0] for company in companies_all]
+        else:
+            raise ValueError("Couldn't find companies ¯\_(ツ)_/¯")
+
     def get_company_info(name):
         name = name.upper()
         cursor.execute(
@@ -62,19 +74,6 @@ class Company:
             return company_id[0]
         else:
             raise ValueError("Could not find the id of company")
-
-    @classmethod
-    def all_companies(cls):
-        cursor.execute(
-            '''
-            SELECT name FROM companies
-            '''
-        )
-        companies_all = cursor.fetchall()
-        if companies_all:
-            return [company[0] for company in companies_all]
-        else:
-            raise ValueError("Couldn't find companies ¯\_(ツ)_/¯")
 
     def get_by_id(company_id):
         company = cursor.execute(
@@ -129,8 +128,68 @@ class Car:
         self.in_pr = in_pr
         self.company_id = company_id
 
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self, value):
+        if type(value) is str:
+            self._model = value
+        else:
+            raise ValueError("Type in characters plz")
 
-fake_company = Company("Packard", 4, 5)
+    @property
+    def typeof(self):
+        return self._typeof
+    @typeof.setter
+    def typeof(self, value):
+        if type(value) is str:
+            self._typeof = value
+        else:
+            raise ValueError("Type in characters, no other data type")
+
+    @property
+    def generations(self):
+        return self._generations
+    @generations.setter
+    def generations(self, value):
+        if type(value) is int:
+            self._generations = value
+        else:
+            raise ValueError("Type in a number, not that you dum dum")
+
+    @property
+    def years(self):
+        return self._years
+    @years.setter
+    def years(self, value):
+        if type(value) is str:
+            self._years = value
+        else:
+            raise ValueError("Please type in valid year info")
+
+    @property
+    def in_pr(self):
+        return self._in_pr
+    @in_pr.setter
+    def in_pr(self, value):
+        if type(value) is bool:
+            self._in_pr = value
+        else:
+            raise ValueError("Needs to be True or false statement")
+
+    @property
+    def company_id(self):
+        return self._company_id
+    @company_id.setter
+    def company_id(self, value):
+        if type(value) is int:
+            self._company_id = value
+        else:
+            raise ValueError("Must be a number, try again bozo")
+
+
+fake_model = Car("Cadillac Fleetwood", "sedan", 5, "1948-present", True, 17)
 
 if __name__ == "__main__":
 
@@ -140,8 +199,8 @@ if __name__ == "__main__":
         options = [
             inquirer.List(
                 "choice",
-                message="Select your next operation",
-                choices=["Choose Car Company", "Search Car Company", "Create Company", "Update Company", "Delete Company", "See Car Model info", "Create Car", "Update Car", "Delete Car", "Exit"],
+                message= "Select your next operation",
+                choices= ["Choose Car Company", "Search Car Company", "Create Company", "Update Company", "Delete Company", "See Car Model info", "Create Car", "Update Car", "Delete Car", "Exit"],
             ),
         ]
         answers = inquirer.prompt(options)
@@ -149,8 +208,8 @@ if __name__ == "__main__":
             car_list = [
                 inquirer.List(
                     "car_choice",
-                    message="Choose a Car company",
-                    choices=Company.all_companies()
+                    message= "Choose a Car company",
+                    choices= Company.all_companies()
                 ),
             ]
             car_answers = inquirer.prompt(car_list)
@@ -161,6 +220,7 @@ if __name__ == "__main__":
             print("Models/Trims currently produced:", manufacture_info[1])
             print("Discontinued Models:", manufacture_info[2])
             print("Models:")
+
         elif answers['choice'] == "Search Car Company":
             typed = input("Type in car company name: ")
             chosen_company_id = Company.get_company_info(typed)
@@ -170,23 +230,24 @@ if __name__ == "__main__":
             print("Models/Trims currently produced:", manufacture_info[1])
             print("Discontinued Models:", manufacture_info[2])
             print("Models:")
+
         elif answers['choice'] == "Create Company":
             new_name = input("Type in Company Name: ")
             new_production = input("Type how many models are being currently produced: ")
             new_discontinued = input("Type how many models have been discontinued: ")
             new_company = Company(new_name, int(new_production), int(new_discontinued))
             new_company.save()
-            conncection.commit()
+
         elif answers['choice'] == "Update Company":
             choose_company = input("Type in company you want to update: ")
             update_name = input("Type in what you want to change the company name to: ").upper()
             chosen_company_id = Company.get_company_info(choose_company)
             Company.update(update_name, chosen_company_id)
-            conncection.commit()
+
         elif answers['choice'] == "Delete Company":
             choose_company = input("Type in which company you would like to delete: ")
             chosen_company_id = Company.get_company_info(choose_company)
             Company.delete(chosen_company_id)
-            conncection.commit()
+
         elif answers['choice'] == "Exit":
             break
